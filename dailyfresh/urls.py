8b317b04426ed7230aae25ecb44be24eacc5c078
jinwang <jinwang@laas.fr>
2022-06-22 17:11:40 +0200

    static

--- dailyfresh/urls.py
+++ dailyfresh/urls.py
@@ -1,18 +1,18 @@
 # -*-coding:utf-8-*-
 from django.contrib import admin
 from django.urls import path, include
-# from django.conf.urls import include
+
 
 from apps.user import views as user_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('tinymce/', include('tinymce.urls')),  # 富文本编辑器
-    # # path('search/', include('haystack.urls')),  # 全文检索框架
-    path('user/', include('user.urls')),  # 用户模块
-    # path('cart/', include('apps.cart.urls')),  # 购物车模块
-    # path('order/', include('apps.order.urls')),  # 订单模块
-    # path('', include('apps.goods.urls')),  # 商品模块
+    path('tinymce/', include('tinymce.urls')),  # 富文本编辑器
+    # path('search/', include('haystack.urls')),  # 全文检索框架
+    # path('user/', include('user.urls')),  # 用户模块
+    path('cart/', include('apps.cart.urls')),  # 购物车模块
+    path('order/', include('apps.order.urls')),  # 订单模块
+    path('', include('apps.goods.urls')),  # 商品模块
 
     # path('admin/', admin.site.urls),
     # path('tinymce/', include('tinymce.urls')),  # 富文本编辑器
