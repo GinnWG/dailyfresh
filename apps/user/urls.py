f687f3a3475e8844212310de050c6be19e491ff4
jinwang <jinwang@laas.fr>
2022-06-28 18:16:56 +0200

    email a faire

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,5 +1,3 @@
-from django.template.defaulttags import url
-
 from apps.user import views
 from django.urls import path, include
 
@@ -7,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.RegisterView.as_view(), name="register"),
+    path("register/", views.RegisterView.as_view(), name="register"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("active/<token>", views.ActiveView.as_view(), name="active"),
     # path('user/register_handle/', views.register_handle, name='register_handle'),
--- apps/user/views.py
+++ apps/user/views.py
@@ -4,59 +4,13 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.views import View
 from django.urls import reverse
+from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.conf import settings
 from itsdangerous import TimedSerializer
 from itsdangerous import SignatureExpired
 
 User = get_user_model()
-
-
-# def register(request):
-#     # load register page
-#     if request.method == 'GET':
-#         return render(request, 'register.html')
-#     # get personnel data from register.html
-#     else:
-#         username = request.POST.get('user_name')
-#         password = request.POST.get('pwd')
-#         password2 = request.POST.get('cpwd')
-#         email = request.POST.get('email')
-#         allow = request.POST.get('allow')
-#
-#         # verify
-#         # data incomplete
-#         if not all([username, password, email]):
-#             return render(request, 'register.html', {'errmsg': 'data incomplete'})
-#         # email
-#         if not re.match(r'^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
-#             return render(request, 'register.html', {'errmsg': 'email form illegal'})
-#
-#         # psw
-#         if not password == password2:
-#             return render(request, 'register.html', {'errmsg': 'password non correspondent'})
-#
-#         if allow != 'on':
-#             return render(request, 'register.html', {'errmsg': 'non accept contract'})
-#
-#         try:
-#             user = User.objects.get(username=username)
-#         except User.DoesNotExist:
-#             # user non exist
-#             user = None
-#         # user exist
-#         if user:
-#             print('===user exist===')
-#             return render(request, 'register.html', {'errmsg': 'user exist'})
-#
-#         print('------------')
-#         user = User.objects.create_user(username, email, password)
-#         user.is_active = 0
-#         user.save()
-#         return redirect(reverse('index'))
-
-# def login(request):
-#     return render(request, 'login.html')
 
 
 class RegisterView(View):
@@ -121,6 +75,12 @@ class ActiveView(View):
             user.is_active = 1
             user.save()
 
+            # send pre user email
+            subject = 'Dailyfresh welcome new client'
+            message = 'Dailyfresh fresh everyday, please active your count'
+            sender = settings.EMAIL_FROM
+            receiver = [User.email]
+            send_mail(subject,message,sender,receiver)
             # redirect to login
             return redirect(reverse('login'))
         except SignatureExpired as err:
