8dbe9e2ce8ee1232f89f215d0ba8c1c05dbad84c
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-06-28 10:42:10 +0200

    fix urls

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,7 +1,8 @@
 
-from apps.goods import views
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
-    path('$/', views.index, name='index'),
+    path('', views.index, name='index'),
 ]
